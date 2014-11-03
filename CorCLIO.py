d1b59c716d8ca41fbb1a64381f984c82f17a296f
John J. Oh / National Institute for Mathematical Sciences / Korean Gravitational Wave Group <john.oh@ui04.sdfarm.kr>
2014-11-03 14:27:39 +0900

    Genralized Code Update

--- CorCLIO.py
+++ CorCLIO.py
@@ -4,38 +4,38 @@ from sklearn.metrics.cluster import normalized_mutual_info_score
 from sklearn.metrics.cluster import mutual_info_score
 from scipy.stats import pearsonr
 
-f=np.loadtxt('1034657674_scatter_maxPearson_norm.txt')
-g=np.loadtxt('1034657674_scatter_norm.txt_maxMIC.txt')
+from os import makedirs
+from os.path import isdir, exists
+from sys import exit
+from optparse import *
 
-print 'Correlation Comparision for 1034657674_scatter_maxPearsonR_norm.txt'
+parser=OptionParser(usage="Computing MIC and PearsonR with a given data", version="1.000")
+parser.add_option("-f","--filename", action="store", type="string", default="NoFile", help="Input data file; Default is NoFile")
+#parser.add_option("-o","--output-dir", action="store", type="string", default="output", help="Output directory; Default is output")
+parser.add_option("-i","--input-dir", action="store", type="string", default=".", help="Input directory; Default is .")
+parser.add_option("-s","--time-shift", action="store", type="int", default="0", help="Time-shift")
 
-print 'Correlation Indices for NO time-shifted between SEIS and CLIO_GW:'
-print 'PearsonR =', pearsonr(f.T[0],f.T[1])
-print 'Normalized MIC =', normalized_mutual_info_score(f.T[0],f.T[1])
-
-
-print 'Correlation Indices for 5s time-shifted between SEIS and CLIO_GW:'
-print 'PearsonR =', pearsonr(f.T[0][5:],f.T[1][:-5])
-print 'Normalized MIC =', normalized_mutual_info_score(f.T[0][5:],f.T[1][:-5])
-
-print 'Correlation Indices for 11s time-shifted between SEIS and CLIO_GW:'
-print 'PearsonR =', pearsonr(f.T[0][11:],f.T[1][:-11])
-print 'Normalized MIC =', normalized_mutual_info_score(f.T[0][11:],f.T[1][:-11])
-
-print 'Correlation Comparision for 1034657674_scatter_norm.txt_maxMIC.txt'
-
-print 'Correlation Indices for NO time-shifted between SEIS and CLIO_GW:'
-print 'PearsonR =', pearsonr(g.T[0],g.T[1])
-print 'Normalized MIC =', normalized_mutual_info_score(g.T[0],g.T[1])
+(opts,files)=parser.parse_args()
+filename   = '.'.join(((opts.filename.split('/'))[-1].split('.'))[:-1])
+input_dir = opts.input_dir
+input_file = input_dir+'/'+opts.filename
+#output_dir = opts.output_dir
+ts=opts.time_shift
 
 
-print 'Correlation Indices for 5s time-shifted between SEIS and CLIO_GW:'
-print 'PearsonR =', pearsonr(g.T[0][5:],g.T[1][:-5])
-print 'Normalized MIC =', normalized_mutual_info_score(g.T[0][5:],g.T[1][:-5])
+f=np.loadtxt(input_dir+'/'+filename+'.txt')
 
-print 'Correlation Indices for 11s time-shifted between SEIS and CLIO_GW:'
-print 'PearsonR =', pearsonr(g.T[0][11:],g.T[1][:-11])
-print 'Normalized MIC =', normalized_mutual_info_score(g.T[0][11:],g.T[1][:-11])
 
+print 'Correlation Comparision for '+filename
+
+if ts == 0:
+    print 'Correlation Indices for NO time-shifted between SEIS and CLIO_GW:'
+    print 'PearsonR =', pearsonr(f.T[0],f.T[1])
+    print 'Normalized MIC =', normalized_mutual_info_score(f.T[0],f.T[1])
+
+else:
+    print 'Correlation Indices for '+ts+'sec. time-shifted between SEIS and CLIO_GW:'
+    print 'PearsonR =', pearsonr(f.T[0][ts:],f.T[1][:-ts])
+    print 'Normalized MIC =', normalized_mutual_info_score(f.T[0][ts:],f.T[1][:-ts])
 
 print 'ALL JOBS DONE!'
